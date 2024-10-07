db1cca84f8cf512e250a6319f94c66fc268f0090
Vladislav Boiko <130917759+Vladislav-Boiko-2005@users.noreply.github.com>
2024-10-07 19:41:58 +0300

    Update 4 Двумерное динамическое программирование.cpp

--- Динамическое Программирование/4 Двумерное динамическое программирование.cpp
+++ Динамическое Программирование/4 Двумерное динамическое программирование.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+int main() {
+	int n,m;
+	cin >> n>>m;
+	vector<vector<long long>> a (n + 1, vector<long long>(m + 1, 1));
+	vector<vector<long long>>dp (n + 1, vector<long long>(m + 1, 0));
 
+	for (auto i = 1; i < n + 1; i++) {
+		for (auto j = 1; j < m + 1; j++) {
+			int vvod;
+			cin >> vvod;
+			a[i][j] = vvod;
+		}
+	}
+
+	dp[0][0] = 1;
+	long long limit = pow(10, 9)+9;
+	for (auto i = 1; i < n + 1; i++) {
+		for (auto j = 1; j < m + 1; j++) {
+			if (a[i][j] == 0) {
+				dp[i][j] = (dp[i - 1][j] + dp[i][j - 1] + dp[i - 1][j - 1])%limit;
+			}
+		}
+	}
+	cout << dp[n][m] << '\n';
+}
